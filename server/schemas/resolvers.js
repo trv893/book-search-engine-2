b6d55b0d6a527b7add48e7fe1cb6ac7b8e4fd5fd
trv893 <trevorhilimire@gmail.com>
2022-08-30 14:56:16 -0400

    package change for heroku19

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
