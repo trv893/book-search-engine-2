8a66d35e757af6df1a5639394778fe811fc3c43e
trv893 <trevorhilimire@gmail.com>
2022-08-30 13:21:42 -0400

    package change for heroku3

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
-require("dotenv").config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
 });
 
 module.exports = mongoose.connection;
